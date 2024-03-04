81545b38973f8dfddc66f8f92a71d4c37b5baaf7
ruha321 <105205825+ruha321@users.noreply.github.com>
2024-03-04 00:33:40 +0000

    足し算と引き算を追加

--- ruhacc.c
+++ ruhacc.c
@@ -6,11 +6,30 @@ int main(int argc, char **argv){
 		fprintf(stderr, "引数の個数が正しくありません\n");
 		return 1;
 	}
-
+	
+	char *p = argv[1];
+	
 	printf(".intel_syntax noprefix\n");
 	printf(".globl main\n");
 	printf("main:\n");
-	printf("  mov rax, %d\n", atoi(argv[1]));
-	printf("  ret\n");
+	printf("	mov rax, %ld\n", strtol(argv[1]));
+	
+	while(*p){
+		if(*p == '+'){
+			p++;
+			printf("	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+		
+		if(*p == '-'){
+			p++;
+			printf("	sub rax, %ld\n", strtol(p, &p, 10));
+		}
+		
+		fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+		return 1;
+	}
+
+	printf("	ret\n");
 	return 0;
 }
